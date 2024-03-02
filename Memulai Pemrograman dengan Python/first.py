62570c6773ccaf6d02ca87b7542860e799769aca
Jack Berck <berckjack@gmail.com>
2024-03-02 11:44:48 +0700

    Upload new files

---
+++ Memulai Pemrograman dengan Python/first.py
@@ -0,0 +1,25 @@
+# Variabel dalam Python:
+# Variabel adalah nama yang digunakan untuk menyimpan nilai di dalam program Python. Nilai tersebut dapat berupa angka, string, daftar, atau tipe data lainnya. Saat Anda memberikan sebuah nilai pada variabel, Python secara otomatis menetapkan tipe data yang sesuai.
+# Contoh:
+# x = 5         # Variabel x menyimpan nilai 5
+# name = "John" # Variabel name menyimpan string "John"
+
+# Input/Output dalam Python:
+# Input: Fungsi input() digunakan untuk menerima input dari pengguna. Input yang diterima selalu dianggap sebagai string.
+# name = input("Masukkan nama Anda: ")
+# Output: Fungsi print() digunakan untuk menampilkan output ke layar.
+# print("Halo,", name)
+
+# Komentar dalam Python:
+# Komentar adalah bagian dari kode yang tidak dieksekusi oleh Python. Komentar digunakan untuk memberikan penjelasan atau dokumentasi tentang bagaimana kode tersebut bekerja. Komentar diawali dengan tanda # dan dapat berada di awal baris atau setelah kode.
+# # Ini adalah sebuah komentar
+# x = 5  # Komentar bisa juga ditulis di samping kode
+# Anda dapat menggunakan multiple-line comment dengan menggunakan tiga tanda kutip (''' atau """) di awal dan akhir komentar. Ini memungkinkan Anda untuk menuliskan komentar dalam beberapa baris.
+# Contoh penggunaan multiple-line comment:
+# '''
+# Ini adalah multiple-line comment.
+# Ini adalah contoh penggunaan multiple-line comment
+# dalam Python. Komentar ini bisa berisi beberapa baris.
+# Komentar ini tidak akan dieksekusi oleh Python.
+# '''
+
